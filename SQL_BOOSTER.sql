81fe4e3e4d92748968da01123283c27b93977828
leechun1095 <github>
2022-03-05 16:33:03 +0900

    2022.03.05

--- SQL_BOOSTER.sql
+++ SQL_BOOSTER.sql
@@ -1631,7 +1631,7 @@ SELECT T1.CUS_ID
 -- SELECT 절의 상관 서브쿼리 인라인-뷰 바깥으로 빼기
 -- 스칼라 서브쿼리 사용 시 GROUP BY에 스칼라 서브쿼리의 조건값을 꼭 넣어줘야한다.
 -- GROUP BY에 서브쿼리 전체 넣으면 에러난다.
-SELECT T1.CUS_ID 
+SELECT T1.CUS_ID
      , SUBSTR(T1.ORD_YMD,1,6) ORD_YM
      , MAX(T1.CUS_NM)
      , MAX(T1.CUS_GD)
@@ -1662,3 +1662,289 @@ SELECT T1.CUS_ID
            AND T1.CUS_ID = T2.CUS_ID
        ) T1
  GROUP BY T1.CUS_ID, SUBSTR(T1.ORD_YMD,1,6), T1.ORD_ST, T1.PAY_TP;
+
+-- ************************************************
+-- PART I - 4.1.3 SQL4
+-- ************************************************
+-- 서브쿼리 안에서 조인을 사용한 예
+SELECT T1.ORD_DT
+     , T2.ORD_QTY
+     , T2.ITM_ID
+     , T3.ITM_NM
+     , (SELECT SUM(B.EVL_PT) / COUNT(*)
+			    FROM M_ITM A
+					   , T_ITM_EVL B
+			   WHERE A.ITM_TP = T3.ITM_TP
+			     AND B.ITM_ID = A.ITM_ID
+			     AND B.EVL_DT < T1.ORD_DT
+		   ) ITM_TP_EVL_PT_AVG
+  FROM T_ORD T1
+		 , T_ORD_DET T2
+		 , M_ITM T3
+ WHERE T1.ORD_DT >= TO_DATE('20170801','YYYYMMDD')
+   AND T1.ORD_DT < TO_DATE('20170901','YYYYMMDD')
+   AND T3.ITM_ID = T2.ITM_ID
+   AND T1.ORD_SEQ = T2.ORD_SEQ
+ ORDER BY T1.ORD_DT, T2.ITM_ID;
+
+-- ************************************************
+-- PART I - 4.1.4 SQL1
+-- ************************************************
+-- 실행이 불가능한 SELECT 절의 서브쿼리
+--SELECT 절의 서브쿼리에서 두 컬럼을 지정.(오류남)
+SELECT T1.ORD_DT ,T1.CUS_ID
+		 , (SELECT A.CUS_NM ,A.CUS_GD FROM M_CUS A WHERE A.CUS_ID = T1.CUS_ID) CUS_NM_GD
+  FROM T_ORD T1
+ WHERE T1.ORD_DT >= TO_DATE('20170401','YYYYMMDD')
+   AND T1.ORD_DT < TO_DATE('20170501','YYYYMMDD');
+
+--SELECT 절의 서브쿼리에서 두 건 이상의 데이터가 나오는 경우.(오류남)
+SELECT T1.ORD_DT ,T1.CUS_ID
+		 , (SELECT A.ITM_ID FROM T_ORD_DET A WHERE A.ORD_SEQ = T1.ORD_SEQ) ITM_LIST
+  FROM T_ORD T1
+ WHERE T1.ORD_DT >= TO_DATE('20170401','YYYYMMDD')
+   AND T1.ORD_DT < TO_DATE('20170501','YYYYMMDD');
+
+-- ************************************************
+-- PART I - 4.1.4 SQL2
+-- ************************************************
+-- 고객 이름과 등급을 합쳐서 하나의 컬럼으로 처리
+-- 단가(UNT_PRC)와 주문수량(ORD_QTY)를 곱해서 주문금액으로 처리.
+SELECT T1.ORD_DT
+     , T1.CUS_ID
+		 , (SELECT A.CUS_NM||'('||CUS_GD||')' FROM M_CUS A WHERE A.CUS_ID = T1.CUS_ID) CUS_NM_GD
+		 , (SELECT SUM(A.UNT_PRC * A.ORD_QTY) FROM T_ORD_DET A WHERE A.ORD_SEQ = T1.ORD_SEQ) ORD_AMT
+  FROM T_ORD T1
+ WHERE T1.ORD_DT >= TO_DATE('20170401','YYYYMMDD')
+   AND T1.ORD_DT < TO_DATE('20170501','YYYYMMDD');
+
+-- ************************************************
+-- PART I - 4.1.4 SQL3
+-- ************************************************
+-- 고객별 마지막 ORD_SEQ의 주문금액
+SELECT T1.CUS_ID
+		 , T1.CUS_NM
+     -- 문자열로 연결하여(||) MAX() 하면 가장 나중의 SEQ가 구해짐.
+		 , (SELECT TO_NUMBER(SUBSTR(MAX(LPAD(TO_CHAR(A.ORD_SEQ),8,'0')||TO_CHAR(A.ORD_AMT)),9))
+					FROM T_ORD A
+         WHERE A.CUS_ID = T1.CUS_ID
+       ) LAST_ORD_AMT
+  FROM M_CUS T1
+ ORDER BY T1.CUS_ID;
+
+-- ************************************************
+-- PART I - 4.1.4 SQL4
+-- ************************************************
+-- 고객별 마지막 ORD_SEQ의 주문금액 – 중첩된 서브쿼리
+SELECT T1.CUS_ID
+		 , T1.CUS_NM
+		 , (
+  			SELECT B.ORD_AMT
+  			  FROM T_ORD B
+  			 WHERE B.ORD_SEQ = (SELECT MAX(A.ORD_SEQ) FROM T_ORD A WHERE A.CUS_ID = T1.CUS_ID)
+			 ) LAST_ORD_AMT
+  FROM M_CUS T1
+ ORDER BY T1.CUS_ID;
+
+-- ************************************************
+-- PART I - 4.1.4 SQL5
+-- ************************************************
+-- 잠재적인 오류가 존재하는 서브쿼리 – 정상 실행
+-- T_ORD 테이블의 PK(ORD_SEQ,	ORD_DET_NO)
+SELECT T1.ORD_DT
+		 , T1.CUS_ID
+		 , (SELECT A.ORD_QTY FROM T_ORD_DET A WHERE A.ORD_SEQ = T1.ORD_SEQ) ORD_AMT
+  FROM T_ORD T1
+ WHERE T1.ORD_SEQ = 2297;
+
+-- ************************************************
+-- PART I - 4.1.4 SQL6
+-- ************************************************
+-- 잠재적인 오류가 존재하는 서브쿼리 – 오류 발생
+--1. 오류가 발생하는 서브쿼리(ORD_SEQ = 2291)
+-- T_ORD 테이블의 PK(ORD_SEQ,	ORD_DET_NO)
+SELECT T1.ORD_DT
+		 , T1.CUS_ID
+		 , (SELECT A.ORD_QTY FROM T_ORD_DET A WHERE A.ORD_SEQ = T1.ORD_SEQ) ORD_AMT
+  FROM T_ORD T1
+ WHERE T1.ORD_SEQ = 2291;
+
+--2. T_ORD_DET에 ORD_SEQ가 2291인 데이터는 두 건이 존재한다.
+SELECT T1.*
+  FROM T_ORD_DET T1
+ WHERE T1.ORD_SEQ = 2291;
+
+-- ************************************************
+-- PART I - 4.1.5 SQL1
+-- ************************************************
+-- 마지막 주문 한 건을 조회하는 SQL, ORD_SEQ가 가장 큰 데이터가 마지막 주문이다.
+SELECT *
+  FROM T_ORD T1
+ WHERE T1.ORD_SEQ = (SELECT MAX(A.ORD_SEQ) FROM T_ORD A);
+
+-- ************************************************
+-- PART I - 4.1.5 SQL2
+-- ************************************************
+-- 마지막 주문 한 건을 조회하는 SQL, ORDER BY와 ROWNUM을 사용
+SELECT *
+  FROM (
+    		SELECT *
+    		  FROM T_ORD T1
+    		 ORDER BY T1.ORD_SEQ DESC
+		   ) A
+ WHERE ROWNUM <= 1;
+
+-- ************************************************
+-- PART I - 4.1.5 SQL3
+-- ************************************************
+-- 마지막 주문 일자의 데이터를 가져오는 SQL
+SELECT *
+  FROM T_ORD T1
+ WHERE T1.ORD_DT = (SELECT MAX(A.ORD_DT) FROM T_ORD A);
+
+-- ************************************************
+-- PART I - 4.1.5 SQL4
+-- ************************************************
+-- 3월 주문 건수가 4건 이상인 고객의 3월달 주문 리스트
+SELECT *
+  FROM T_ORD T1
+ WHERE T1.ORD_DT >= TO_DATE('20170301','YYYYMMDD')
+   AND T1.ORD_DT < TO_DATE('20170401','YYYYMMDD')
+   AND T1.CUS_ID IN (
+              			 SELECT A.CUS_ID
+              			   FROM T_ORD A
+              			  WHERE A.ORD_DT >= TO_DATE('20170301','YYYYMMDD')
+              			    AND A.ORD_DT < TO_DATE('20170401','YYYYMMDD')
+              			  GROUP BY A.CUS_ID
+              			 HAVING COUNT(*)>=4
+              			);
+
+-- ************************************************
+-- PART I - 4.1.5 SQL5
+-- ************************************************
+-- 3월 주문 건수가 4건 이상인 고객의 3월달 주문 리스트 – 조인으로 처리
+-- 4건 이상인 고객이 5명이므로 전체(T_DRD A)에서 고객 5명에 대한 데이터만 이너 조인되므로 조건절과 동일한 결과를 가져옴
+SELECT T1.*
+  FROM T_ORD T1
+		 , (
+  			SELECT A.CUS_ID
+  			  FROM T_ORD A
+  			 WHERE A.ORD_DT >= TO_DATE('20170301','YYYYMMDD')
+  			   AND A.ORD_DT < TO_DATE('20170401','YYYYMMDD')
+  			 GROUP BY A.CUS_ID
+  			HAVING COUNT(*)>=4
+  		 ) T2
+ WHERE T1.ORD_DT >= TO_DATE('20170301','YYYYMMDD')
+   AND T1.ORD_DT < TO_DATE('20170401','YYYYMMDD')
+   AND T1.CUS_ID = T2.CUS_ID;
+
+-- ANSI
+SELECT T1.*
+  FROM T_ORD T1
+ INNER JOIN (
+             SELECT A.CUS_ID
+               FROM T_ORD A
+              WHERE A.ORD_DT >= TO_DATE('20170301','YYYYMMDD')
+                AND A.ORD_DT < TO_DATE('20170401','YYYYMMDD')
+              GROUP BY A.CUS_ID
+             HAVING COUNT(*)>=4
+            ) T2
+    ON T1.CUS_ID = T2.CUS_ID
+ WHERE T1.ORD_DT >= TO_DATE('20170301','YYYYMMDD')
+   AND T1.ORD_DT < TO_DATE('20170401','YYYYMMDD');
+
+-- ************************************************
+-- PART I - 4.1.6 SQL1
+-- ************************************************
+-- 3월에 주문이 존재하는 고객들을 조회
+SELECT *
+  FROM M_CUS T1
+ WHERE EXISTS(
+              SELECT *
+                FROM T_ORD A
+               WHERE A.CUS_ID = T1.CUS_ID
+                 AND A.ORD_DT >= TO_DATE('20170301','YYYYMMDD')
+                 AND A.ORD_DT < TO_DATE('20170401','YYYYMMDD')
+             );
+
+-- ************************************************
+-- PART I - 4.1.6 SQL2
+-- ************************************************
+-- 3월에 ELEC 아이템유형의 주문이 존재하는 고객들을 조회
+SELECT *
+  FROM M_CUS T1
+ WHERE EXISTS(
+        		  SELECT *
+        		    FROM T_ORD A
+        				   , T_ORD_DET B
+        				   , M_ITM C
+        		   WHERE A.CUS_ID = T1.CUS_ID
+        		     AND A.ORD_DT >= TO_DATE('20170301','YYYYMMDD')
+        		     AND A.ORD_DT < TO_DATE('20170401','YYYYMMDD')
+        		     AND A.ORD_SEQ = B.ORD_SEQ
+        		     AND B.ITM_ID = C.ITM_ID
+        		     AND C.ITM_TP = 'ELEC'
+        		 );
+
+-- ************************************************
+-- PART I - 4.1.6 SQL3
+-- ************************************************
+-- 전체 고객을 조회, 3월에 주문이 존재하는지 여부를 같이 보여줌
+SELECT T1.CUS_ID ,T1.CUS_NM
+		 , (CASE WHEN
+				  EXISTS(
+    					   SELECT *
+    					     FROM T_ORD A
+    					    WHERE A.CUS_ID = T1.CUS_ID
+    					      AND A.ORD_DT >= TO_DATE('20170301','YYYYMMDD')
+    					      AND A.ORD_DT < TO_DATE('20170401','YYYYMMDD')
+    					  )
+		    THEN 'Y'
+		    ELSE 'N' END
+       ) ORD_YN_03
+  FROM M_CUS T1;
+
+-- ************************************************
+-- PART I - 4.2.1 SQL1
+-- ************************************************
+-- MERGE 문을 위한 테스트 테이블 생성
+CREATE TABLE M_CUS_CUD_TEST AS
+SELECT *
+  FROM M_CUS T1;
+
+ALTER TABLE M_CUS_CUD_TEST
+	ADD CONSTRAINT PK_M_CUS_CUD_TEST PRIMARY KEY(CUS_ID) USING INDEX;
+
+-- ************************************************
+-- PART I - 4.2.1 SQL2
+-- ************************************************
+-- CUS_0090 고객을 입력하거나 변경하는 PL/SQL
+SET ECHO ON
+SET TAB OFF
+SET SERVEROUTPUT ON
+
+DECLARE v_EXISTS_YN varchar2(1);
+
+BEGIN
+	SELECT NVL(MAX('Y'),'N')
+	  INTO v_EXISTS_YN
+	  FROM DUAL A
+	 WHERE EXISTS(
+        			  SELECT *
+        			    FROM M_CUS_CUD_TEST T1
+        			   WHERE T1.CUS_ID = 'CUS_0090'
+			         );
+
+  IF v_EXISTS_YN = 'N' THEN
+    INSERT INTO M_CUS_CUD_TEST (CUS_ID ,CUS_NM ,CUS_GD)
+		VALUES ('CUS_0090' ,'NAME_0090' ,'A');
+		DBMS_OUTPUT.PUT_LINE('INSERT NEW CUST');
+	ELSE
+		UPDATE M_CUS_CUD_TEST T1
+		   SET T1.CUS_NM = 'NAME_0090'
+				 , T1.CUS_GD = 'A'
+		 WHERE CUS_ID = 'CUS_0090';
+		DBMS_OUTPUT.PUT_LINE('UPDATE OLD CUST');
+	END IF;
+	COMMIT;
+END;
